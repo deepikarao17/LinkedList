6129d13417b8787d95ace0f4cd9277314ccdba2e
deepikarao17 <deepika.rao_ccv17@gla.ac.in>
2019-02-20 11:37:25 +0530

    implement MyLinkedList

--- src/main/MyLinkedList.java
+++ src/main/MyLinkedList.java
@@ -34,5 +34,11 @@ public class MyLinkedList {
         this.tail=null;
         currentsize=0;
     }
+
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+        Node node = new Node(10,null);
+//        linkedList.insert(node);
+    }
 }
 
