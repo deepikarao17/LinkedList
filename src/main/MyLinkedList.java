20cf11b99d98d4f9ad03bf26cd699a8db03e35a3
deepikarao17 <deepika.rao_ccv17@gla.ac.in>
2019-02-20 11:26:44 +0530

    add getter n setter

--- src/main/MyLinkedList.java
+++ src/main/MyLinkedList.java
@@ -4,4 +4,28 @@ public class MyLinkedList {
     private Node head;
     private Node tail;
     private int currentsize;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    public int getCurrentsize() {
+        return currentsize;
+    }
+
+    public void setCurrentsize(int currentsize) {
+        this.currentsize = currentsize;
+    }
 }
